5c2bdfa536321fcaa14c58ad46821ef8a284735d
anthony0625 <anthonykntk@gmail.com>
2023-03-14 04:59:32 +0900

    generalized save directory

--- downloader.py
+++ downloader.py
@@ -1,7 +1,5 @@
 import yt_dlp
 import os
-import time
-import subprocess
 
 class YoutubeDownloader:
     def get_playlist_videos(self, playlist_url):
@@ -18,7 +16,7 @@ class YoutubeDownloader:
                     video_urls.append(video_info)
         return video_urls
 
-    def download_video(self, video_id, title):
+    def download_video(self, video_id, title, output_dir):
         video_url = f'https://www.youtube.com/watch?v={video_id}'
         ydl_opts = {'outtmpl': 'temp_video.webm', 'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
                     'ignoreerrors': True, 'quiet': True}
@@ -30,22 +28,19 @@ class YoutubeDownloader:
             os.remove('temp_video.webm.mp4')
             return False
 
-        output_dir = os.path.join(os.path.expanduser('~'), 'Music')
-        title = title.replace('/', '_')  # replace forward slashes with underscores
-        output_path = os.path.normpath(os.path.join(output_dir, f'{title}.wav'))  # sanitize the path
-        os.makedirs(output_dir, exist_ok=True)  # create directory if it doesn't exist
-
-        # Modify this line to add -loglevel panic option to ffmpeg command
+        title = title.replace('/', '_')
+        output_path = os.path.normpath(os.path.join(output_dir, f'{title}.wav'))
+        os.makedirs(output_dir, exist_ok=True)
         os.system(f'ffmpeg -loglevel panic -y -i temp_video.webm.mp4 -acodec pcm_s16le -ar 44100 -ac 2 "{output_path}"')
 
         os.remove('temp_video.webm.mp4')
         return True
 
-    def download_playlist(self, playlist_url):
+    def download_playlist(self, playlist_url, output_dir):
         video_urls = self.get_playlist_videos(playlist_url)
         self.counter = 1
         for video_info in video_urls:
-            if self.download_video(video_info['id'], video_info['title']):
+            if self.download_video(video_info['id'], video_info['title'], output_dir):
                 print('Downloaded video {} of {}'.format(self.counter, len(video_urls)))
                 self.counter += 1
             else:
@@ -55,9 +50,8 @@ class YoutubeDownloader:
 def main():
     downloader = YoutubeDownloader()
     playlist_url = input('Enter YouTube playlist URL: ')
-    downloader.download_playlist(playlist_url)
+    output_dir = input('Enter output directory path: ')
+    output_dir = os.path.normpath(os.path.expanduser(output_dir))
+    downloader.download_playlist(playlist_url, output_dir)
 
 main()
-
-#if __name__ == '__main__':
-#    main()
