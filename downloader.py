51a678588809601a360d9fee4f9d143f2c38149f
anthony0625 <anthonykntk@gmail.com>
2023-03-14 03:37:04 +0900

    fixed ffmpeg issue

--- downloader.py
+++ downloader.py
@@ -1,5 +1,7 @@
 import yt_dlp
 import os
+import time
+import subprocess
 
 class YoutubeDownloader:
     def get_playlist_videos(self, playlist_url):
@@ -8,41 +10,51 @@ class YoutubeDownloader:
             playlist_dict = ydl.extract_info(playlist_url, download=False)
             video_urls = []
             for video in playlist_dict['entries']:
-                if 'url' in video:
-                    video_urls.append(video['url'])
-        print(video_urls)
+                if 'id' in video.keys() and 'title' in video.keys():
+                    video_info = {
+                        'title': video['title'],
+                        'id': video['id']
+                    }
+                    video_urls.append(video_info)
         return video_urls
 
-    def download_video(self, video_url):
-        ydl_opts = {'outtmpl': 'temp_video.%(ext)s', 'ignoreerrors': True, 'quiet': True}
+    def download_video(self, video_id):
+        video_url = f'https://www.youtube.com/watch?v={video_id}'
+        ydl_opts = {'outtmpl': 'temp_video.webm', 'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+                    'ignoreerrors': True, 'quiet': True}
         try:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 ydl.download([video_url])
         except yt_dlp.utils.DownloadError:
-            os.remove('temp_video.mp4')
+            print(f'Error downloading video with ID {video_id}')
+            os.remove('temp_video.webm.mp4')
             return False
 
-        os.system('ffmpeg -i temp_video.mp4 -acodec pcm_s16le -ar 44100 -ac 2 ~/Music/output{}.wav'.format(self.counter))
-        os.remove('temp_video.mp4')
-        self.counter += 1
+        print(f'Converting video with ID {video_id} to WAV format')
+        time.sleep(2)
+        os.system('ffmpeg -i temp_video.webm.mp4 -acodec pcm_s16le -ar 44100 -ac 2 ~/Music/output{}.wav'.format(self.counter))
+        os.remove('temp_video.webm.mp4')
         return True
 
     def download_playlist(self, playlist_url):
         video_urls = self.get_playlist_videos(playlist_url)
         self.counter = 1
-        for url in video_urls:
-            if self.download_video(url):
-                print('Downloaded video {} of {}'.format(self.counter-1, len(video_urls)))
+        for video_info in video_urls:
+            if self.download_video(video_info['id']):
+                print('Downloaded video {} of {}'.format(self.counter, len(video_urls)))
+                self.counter += 1
             else:
-                print('Error downloading video at URL {}'.format(url))
+                print('Error downloading video with ID {}'.format(video_info['id']))
         print('Finished downloading playlist.')
+        print('Saved to ~/Music')
 
 def main():
     downloader = YoutubeDownloader()
     playlist_url = input('Enter YouTube playlist URL: ')
     downloader.download_playlist(playlist_url)
     print("Download complete.")
-    #input('Download complete. Press Enter to exit.')
 
-if __name__ == '__main__':
-    main()
+main()
+
+#if __name__ == '__main__':
+#    main()
